8021d8b8c506d05abde3a76cb7feeba970492a6b
Bcofrex <cofrebenja321@gmail.com>
2024-10-17 16:41:38 -0300

    realizado

--- src/app/pages/auth/sign-up/sign-up.page.ts
+++ src/app/pages/auth/sign-up/sign-up.page.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-sign-up',
@@ -9,19 +10,35 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
 export class SignUpPage implements OnInit {
 
   form = new FormGroup({
-    email: new FormControl('', [Validators.required, Validators.email]),
-    password: new FormControl('', [Validators.required]),
     name: new FormControl('', [Validators.required, Validators.minLength(5)]),
+    email: new FormControl('', [Validators.required, Validators.email]),
     phone: new FormControl('', [Validators.required, Validators.minLength(9)]),
-    address: new FormControl('', [Validators.required, Validators.minLength(15)])
-  })
+    address: new FormControl('', [Validators.required, Validators.minLength(15)]),
+    password: new FormControl('', [Validators.required]),
+    confirmPassword: new FormControl('', [Validators.required])
+  });
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit() {
   }
 
-  submit(){
-    console.log(this.form.value);
+  submit() {
+    if (this.form.valid) {
+      // El formulario es válido, imprimimos los datos en JSON
+      const formData = this.form.value;
+      console.log('Datos del usuario en formato JSON:', JSON.stringify(formData));
+
+      this.router.navigate(['/auth'])
+    } else {
+      // El formulario no es válido, imprimimos los errores específicos
+      console.log('El formulario no es válido. Errores:');
+      Object.keys(this.form.controls).forEach(key => {
+        const controlErrors = this.form.get(key)?.errors;
+        if (controlErrors) {
+          console.log(`Error en ${key}:`, controlErrors);
+        }
+      });
+    }
   }
 }
