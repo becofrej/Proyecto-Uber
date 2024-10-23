402f166cfe19aeb1f495e203b525673ca8abf897
David Rojas <indior.dev@gmail.com>
2024-10-22 22:15:22 -0300

    cambios en colores de botón

--- src/app/pages/auth/forgot-password/forgot-password.page.ts
+++ src/app/pages/auth/forgot-password/forgot-password.page.ts
@@ -1,5 +1,7 @@
+import { FirebaseService } from 'src/app/services/firebase.service';
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { HelperService } from 'src/app/services/helper.service';  // Servicio para mostrar alertas
 
 @Component({
   selector: 'app-forgot-password',
@@ -7,20 +9,31 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
   styleUrls: ['./forgot-password.page.scss'],
 })
 export class ForgotPasswordPage implements OnInit {
-    form = new FormGroup({
+  form = new FormGroup({
     email: new FormControl('', [Validators.required, Validators.email]),
-    password: new FormControl('', [Validators.required]),
-    name: new FormControl('', [Validators.required, Validators.minLength(5)]),
-    phone: new FormControl('', [Validators.required, Validators.minLength(9)]),
-    address: new FormControl('', [Validators.required, Validators.minLength(15)])
-  })
+  });
 
-  constructor() { }
+  constructor(private firebase: FirebaseService, private helper: HelperService) {}
 
-  ngOnInit() {
-  }
+  ngOnInit() {}
 
-  submit(){
-    console.log(this.form.value);
+  async submit() {
+    // Marcar el campo como tocado para mostrar los errores si los hay
+    this.form.markAllAsTouched();
+
+    if (this.form.valid) {
+      const loader = await this.helper.showLoader('Enviando solicitud...');
+      try {
+        await this.firebase.resetPassWord(this.form.value.email);
+        await this.helper.showAlert('Se ha enviado un correo de recuperación de contraseña.', 'Recuperación de contraseña');
+      } catch (error) {
+        console.error('Error al enviar la solicitud de recuperación de contraseña:', error);
+        await this.helper.showAlert('Ocurrió un error al intentar recuperar la contraseña. Inténtalo de nuevo.', 'Error');
+      } finally {
+        loader.dismiss();
+      }
+    } else {
+      console.log('Formulario inválido. Por favor revise los campos.');
+    }
   }
 }
