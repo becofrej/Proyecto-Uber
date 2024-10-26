4ad22aa0d345297ec9bf291efc220b50321a338f
David Rojas <indior.dev@gmail.com>
2024-10-26 06:09:39 -0300

    Actualizacion en la vista de viajes
    ViajeService:
    
    Añadido el método obtenerViajesPorUsuario para obtener los viajes asociados al ID del usuario actual usando el token.
    
    TripPage (trip.page.ts):
    
    -Integración del método obtenerViajesPorUsuario para cargar los viajes específicos del usuario actual.
    -Carga de datos del usuario desde el almacenamiento local (userId y token).
    -Manejo de los viajes obtenidos para mostrarlos en la interfaz.
    
    TripPage (trip.page.html):
    
    -Diseño actualizado para listar los viajes del usuario, incluyendo imagen del vehículo, ubicación de origen, destino, fecha y costo.
    
    -Agregado un mensaje para indicar cuando el usuario no tiene viajes registrados.

--- src/app/services/viaje.service.ts
+++ src/app/services/viaje.service.ts
@@ -1,6 +1,6 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { lastValueFrom } from 'rxjs';
+import { Observable, lastValueFrom } from 'rxjs';
 import { environment } from 'src/environments/environment';
 
 @Injectable({
@@ -8,9 +8,9 @@ import { environment } from 'src/environments/environment';
 })
 export class ViajeService {
 
-  constructor(private http:HttpClient) { }
+  constructor(private http: HttpClient) { }
 
-
+  // Método para agregar viaje
   async agregarViaje(data: bodyViaje) {
     try {
       const response = await lastValueFrom(
@@ -21,9 +21,18 @@ export class ViajeService {
       throw error;
     }
   }
+
+  // Método para obtener viajes por usuario
+  obtenerViajesPorUsuario(userId: number, token: string): Observable<EndPointViaje> {
+    const params = new HttpParams()
+      .set('p_id_usuario', userId.toString())
+      .set('token', token);
+
+    return this.http.get<EndPointViaje>(`${environment.apiUrl}/viaje/obtener`, { params });
+  }
 }
 
-// Definición de la interfaz bodyViaje para el método agregarViaje
+// Interfaces
 interface bodyViaje {
   p_id_usuario: number;
   p_ubicacion_origen: string;
@@ -31,4 +40,26 @@ interface bodyViaje {
   p_costo: number;
   p_id_vehiculo: number;
   token: string;
+}
+
+interface EndPointViaje {
+  message: string;
+  data: {
+    id: number;
+    id_usuario: number;
+    id_vehiculo: number;
+    ubicacion_origen: string;
+    ubicacion_destino: string;
+    fecha: Date;
+    costo: number;
+    nombre_proyecto: string;
+    patente: string;
+    marca: string;
+    modelo: string;
+    anio: number;
+    color: string;
+    tipo_combustible: string;
+    capacidad_pasajeros: number;
+    imagen_vehiculo: string;
+  }[];
 }
